b7f917426f52f999a245fc44f9566c67b46a0ecd
vk7050 <84660046+vk7050@users.noreply.github.com>
2022-07-05 01:06:43 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,3 +8,24 @@ struct Node
     next=NULL;
   }
 };
+
+void print(Node *head)
+{
+  while(head!=NULL)
+  {
+    cout<<head->data<<" ";
+    head=head->next;
+  }
+}
+
+int main()
+{
+  int n;
+  cin>>n;
+  Node *head=NULL;
+  head=new Node(10);
+  head->next=new Node(20);
+  head->next=new Node(30);
+  print(head);
+  return 0;
+}
